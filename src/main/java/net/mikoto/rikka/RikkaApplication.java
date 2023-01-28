2f094c62c3464ce709d086bbf4d7b5c89e52c746
mikoto <mikoto@ungine.cn>
2023-01-29 03:16:22 +0800

    init

---
+++ src/main/java/net/mikoto/rikka/RikkaApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.rikka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RikkaApplication.class, args);
+    }
+
+}
